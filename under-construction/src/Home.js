1442d7466206a265a81c1f102e2ac519da14c8b6
mica-oz <kahulaoz@gmail.com>
2023-08-01 20:24:05 -0600

    home page created

---
+++ under-construction/src/Home.js
@@ -0,0 +1,23 @@
+/* eslint-disable react/jsx-no-comment-textnodes */
+
+import React, { useEffect, useRef } from 'react';
+
+import './home.css';
+
+function Home() {
+
+
+ 
+
+  return (
+    <div className='flex'>
+    <div className="App">
+   <div className="Row1"></div>
+   <div className="Row2"></div>
+   <div className="Row3"></div>
+    </div>
+    </div>
+  );
+}
+
+export default Home;
